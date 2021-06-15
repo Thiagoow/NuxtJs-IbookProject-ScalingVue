ebfb7765b8b9037974ead2c89877ede7ec035e2f
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-06-15 14:01:00 -0300

    Na aula de bóson de paleta de cores

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['normalize.css/normalize.css', '@/assets/scss/base.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
