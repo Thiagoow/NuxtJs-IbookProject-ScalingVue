f3e7c11d7af27de68d12434b10d394b56d1e38d1
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-06-21 17:05:22 -0300

    Erro no módulo de books :/

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,15 @@
 import Vue from 'vue'
 // -----------Fazendo o SSR (SEMPRE NAS PAGES):
 // 1º Importamos o modulo definido na pasta store:
+import { books } from '@/store'
 
 export default Vue.extend({
   layout: 'iBook',
   /* Testando o módulo:
   (sendo esse módulo assíncrono para a página
   só ser exibida depois que os livros carregarem): */
+  async asyncData() {
+    await books.index()
+  },
 })
 </script>
